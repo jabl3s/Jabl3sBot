0055fb8705c7d798bfaf6e5b7f5f5ff40ebb6c33
J <j@jabl3s.uk>
2023-09-05 03:59:20 +0100

    threading

--- code-discord/code/sub-code/BotDiscord.py
+++ code-discord/code/sub-code/BotDiscord.py
@@ -1,16 +1,33 @@
 import discord
+from decouple import config
+import pika
+import threading
 
 class BotDiscord(discord.Client):
+    def __init__(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(config('RABBIT_MQ_URL')))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue='Twitch')
+        self.channel.queue_declare(queue='Discord')
+        self.channel.basic_consume(queue='Twitch',
+                      auto_ack=True,
+                      on_message_callback=self.on_twitch)
     async def on_ready(self):
         print('Logged on as', self.user)
 
     async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
+        if message.author.name == "jabl3s":
+            self.channel.basic_publish(exchange='',
+                routing_key='Twitch',
+                body=message.content)
+            print("jabl3s: ", message.content)
+        else:
             return
-
-        if message.content == 'ping':
-            await message.channel.send('pong')
+        #if message.content == 'ping':
+        #    await message.channel.send('pong')
+    async def on_twitch(self, ch, method, properties, body):
+        channel = self.get_channel(1148445315326820373)
+        await channel.send(body)
 
 intents = discord.Intents.default()
 intents.message_content = True
--- code-twitch/code/sub-code/BotTwitch.py
+++ code-twitch/code/sub-code/BotTwitch.py
@@ -1,12 +1,22 @@
 import sys
 from twitchio.ext import commands
+from decouple import config
+import pika
+import threading
 
 class BotTwitch(commands.Bot):
-
     def __init__(self, jtokenedit, jchannelsedit):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         # prefix can be a callable, which returns a list of strings or a string...
         # initial_channels can also be a callable which returns a list of strings...
+        connection = pika.BlockingConnection(pika.ConnectionParameters(config('RABBIT_MQ_URL')))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue='Twitch')
+        self.channel.queue_declare(queue='Discord')
+        self.bot_account_name = "jabl3s_ttv_bot"
+        self.channel.basic_consume(queue='Discord',
+                      auto_ack=True,
+                      on_message_callback=self.send_message)
         super().__init__(token=jtokenedit, prefix='?', initial_channels=jchannelsedit)
 
     async def event_ready(self):
@@ -14,29 +24,37 @@ class BotTwitch(commands.Bot):
         # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
+    
+    async def send_message(self, ch, method, properties, body):
+        #if message.author.name.lower() != self.bot_account_name.lower():
+        ws = self._ws
+        await ws.send_privmsg('jabl3s_ttv', body)
+
 
     async def event_message(self, message):
         # Messages with echo set to True are messages sent by the bot...
         # For now we just want to ignore them...
         if message.echo:
             return
-
-        # Print the contents of our message to console...
-        print(message.content)
+        temp=message.author.name+" - "+message.content
+        self.channel.basic_publish(exchange='',
+            routing_key='Discord',
+            body=temp)
+        print(" Twitch: ",temp)
         sys.stdout.flush()
-
+    
         # Since we have commands and are overriding the default `event_message`
         # We must let the bot know we want to handle and invoke our commands...
-        await self.handle_commands(message)
+        #await self.handle_commands(message)
 
-    @commands.command()
-    async def hello(self, ctx: commands.Context):
+    #@commands.command()
+    #async def hello(self, ctx: commands.Context):
         # Here we have a command hello, we can invoke our command with our prefix and command name
         # e.g ?hello
         # We can also give our commands aliases (different names) to invoke with.
 
         # Send a hello back!
         # Sending a reply back to the channel is easy... Below is an example.
-        await ctx.send(f'Hello {ctx.author.name}!')
+        #await ctx.send(f'Hello {ctx.author.name}!')
 
 # bot.run() is blocking and will stop execution of any below code here until stopped or closed.
