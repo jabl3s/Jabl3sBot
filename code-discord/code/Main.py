fb2905be4a6fc8b618c0350eb9a187942539a076
J <j@jabl3s.uk>
2023-09-24 20:30:13 +0100

    module folder same as main

--- code-discord/code/Main.py
+++ code-discord/code/Main.py
@@ -7,7 +7,7 @@ def main():
     BOT_DISCORD_TOKEN = config('BOT_DISCORD_TOKEN')
     bot={}
     # Specify the folder containing your modules
-    module_folder = "sub-code"
+    module_folder = ""
     # Get a list of all Python files in the subfolder (excluding __init__.py)
     module_files = [
         f.replace(".py", "")
